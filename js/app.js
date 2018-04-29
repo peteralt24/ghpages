a820a7715e5a9f24f8914af8b79188cc0fbd5594
peteralt24 <38824287+peteralt24@users.noreply.github.com>
2018-04-28 23:20:13 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -122,7 +122,7 @@ particlesJS('particles-js',
     "retina_detect": true,
     "config_demo": {
       "hide_card": false,
-      "background_color": "##062f4f",
+      "background_color": "#062f4f",
       "background_image": "",
       "background_position": "50% 50%",
       "background_repeat": "no-repeat",
